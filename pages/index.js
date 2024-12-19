0176a5d7bd2cdc541c39b2377e8d73086e697712
Mateus Ferreira Freitas <107125843+FerreiraMF@users.noreply.github.com>
2024-12-19 19:44:58 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Você é a razão pela qual todos os meus dias têm mais brilho e meu coração tem mais paz; amo você mais do que palavras podem expressar.</h1>
+    
+}
+git 
+export default Home;
