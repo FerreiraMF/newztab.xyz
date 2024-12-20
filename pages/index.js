290fe61b29f34b24f068346993d02c1303798771
Mateus Ferreira Freitas <107125843+FerreiraMF@users.noreply.github.com>
2024-12-20 20:06:42 +0000

    corrigindo frase da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é a razão pela qual todos os meus dias têm mais brilho e meu coração tem mais paz; amo você mais do que palavras podem expressar.</h1>
+    return <h1> Sarah Você é a razão pela qual todos os meus dias têm mais brilho e meu coração tem mais paz; amo você mais do que palavras podem expressar.</h1>
     
 }
 
