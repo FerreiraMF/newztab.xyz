fdee482daaf931328b5069664c2aceb8a4697b5a
Mateus Ferreira Freitas <107125843+FerreiraMF@users.noreply.github.com>
2024-12-21 23:24:57 +0000

    adiciona mais uma frase a página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1> Sarah Você é a razão pela qual todos os meus dias têm mais brilho e meu coração tem mais paz; amo você mais do que palavras podem expressar.</h1>
+    return <h1> Sarah Você é a razão pela qual todos os meus dias têm mais brilho e meu coração tem mais paz; amo você mais do que palavras podem expressar. 
+        E para a minha filha Sofia: você me fez cresçer muito depois que você nasceu</h1>
     
 }
 
