e04df295f580634e1bfdac8c585e20c3456a24d5
Mateus Ferreira Freitas <107125843+FerreiraMF@users.noreply.github.com>
2024-12-25 03:54:17 +0000

    alterou a mensagem da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      {" "}
-      Sarah Você é a razão pela qual todos os meus dias têm mais brilho e meu
-      coração tem mais paz; amo você mais do que palavras podem expressar. E
-      para a minha filha Sofia: você me fez cresçer muito depois que você nasceu
-    </h1>
-  );
+  return <h1> Feliz Natal Coroa, Amamos o Senhor! Ass: Do seu FIlho Mateus</h1>;
 }
 
 export default Home;
